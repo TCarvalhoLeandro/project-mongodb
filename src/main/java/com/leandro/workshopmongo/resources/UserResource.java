648570965ad6e035cef4e713c037efd0b6e7269b
Leandro <trairicarvalho@gmail.com>
2026-03-11 13:39:50 -0300

    feat: cria padrão DTO para restornar User

--- src/main/java/com/leandro/workshopmongo/resources/UserResource.java
+++ src/main/java/com/leandro/workshopmongo/resources/UserResource.java
@@ -2,6 +2,7 @@ package com.leandro.workshopmongo.resources;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.leandro.workshopmongo.domain.User;
+import com.leandro.workshopmongo.dto.UserDTO;
 import com.leandro.workshopmongo.services.UserService;
 
 @RestController
@@ -20,10 +22,16 @@ public class UserResource {
 	private UserService userService;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		
-		List<User> list = userService.findAll();
-		return ResponseEntity.ok().body(list);
+		List<User> list = userService.findAll();// recebe uma lista de User que vem la do banco
+		List<UserDTO> listDTO = list.stream().map(obj -> new UserDTO(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDTO);
 	}
 	
 }
+
+//.strem() -> Transforma os dados da list em uma coleção compativel com expressões lambda
+//.map() -> Pega cada Objeto obj da lista e transforma em um novo UserDto passando obj como argumento
+//.collect() -> Transforma de volta em uma lista
